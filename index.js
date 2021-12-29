b6d0b7f7f1e1b2976db827f937b6b801ab7949cb
DylanWard14 <dylan.ward@iinet.net.au>
2021-12-29 22:30:02 +1000

    Created docker-compose file and connected application to redis store

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const redis = require("redis");
 
 const app = express();
-const client = redis.createClient();
+// TODO migrate redis implementation to newest version
+const client = redis.createClient({
+  host: "redis-server",
+  port: 6379,
+});
+
 client.set("visits", 0);
 
 app.get("/", (req, res) => {
